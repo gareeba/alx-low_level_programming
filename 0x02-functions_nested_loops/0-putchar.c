9b857a6de6317c5691f28d5424ef1348a77afe16
gareeba <jasiecrystal@gmail.com>
2022-03-16 14:46:36 -0700

    My putchar task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
-*main - Entry point
+*main - print _putchar.
 *
-*Return: nothing, return void
+*Return: success always.
 */
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('h');
-_putchar('a');
-_putchar('r');
+int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+int count, sz;
+sz = sizeof(str) / sizeof(int);
+for (count = 0; count < sz ; count++)\
+{
+_putchar(str[count]);
+}
 _putchar('\n');
-return(0);
-::}
+return (0);
+} 
