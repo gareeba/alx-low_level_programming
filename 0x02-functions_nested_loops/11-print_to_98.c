027389518203561d325a28bcff6b716f9ce4830a
gareeba <jasiecrystal@gmail.com>
2022-03-16 18:41:39 -0700

    My eleventh task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,7 +3,7 @@
 /**
 *print_to_98 - a function that prints all natural numbers from n to 98
 *user input's number prints to 98, regardless < 98 or > 98
-@n: number input
+*@n: number input
 *Return: Always 0 (success)
 */
 void print_to_98(int n)
