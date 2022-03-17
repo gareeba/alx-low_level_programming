e988d972aae6ebb3ecde58c25d971c477587d1c4
gareeba <jasiecrystal@gmail.com>
2022-03-16 18:32:49 -0700

    My eleventh task

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,10 +12,10 @@ if (n < 98)
 {
 while (n <= 98)
 {
-printf("%d"' n);
+printf("%d", n);
 if (n != 98)
 {
-printf(",");
+printf(", ");
 }
 n++;
 }
@@ -25,9 +25,9 @@ else if (n > 98)
 while (n >= 98)
 {
 printf("%d", n);
-if (*n != 98)
+if (n != 98)
 {
-printf(",");
+printf(", ");
 }
 n--;
 }
